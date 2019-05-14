499aa6c9e1d9ddb90cafe7a9c364f549ab2a6f1c
Polonaaaa <polona.polonaca@gmail.com>
2019-05-14 16:32:46 +0200

    se nekaj funkcij

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 STEVILO_DOVOLJENIH_NAPAK = 10
-PRAVILNA_CRKA = '+',
+PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
 
@@ -12,27 +12,60 @@ class Igra:
         self.crke = []
     
     def napacne_crke(self):
-        return [c for c in self.crke if c not in self.geslo]
+        return [c for c in self.crke if c not in self.geslo.upper()]
 
     def pravilne_crke(self):
-        return [c for c in self.crke if c in self.geslo]
+        return [c for c in self.crke if c in self.geslo.upper()]
 
     def stevilo_napak(self):
         return len(self.napacne_crke())
 
     def zmaga(self):
-        return all(c in self.crke for c in self.geslo)
+        return all(c in self.crke for c in self.geslo.upper())
 
     def poraz(self):
         return self.stevilo_napak() >= STEVILO_DOVOLJENIH_NAPAK
 
+    def pravilni_del_gesla(self):
+        novi = ''
+        for c in self.geslo:
+            if c in self.crke:
+                novi += c
+            else :
+                novi += '_'
+        return novi
+    
+    def nepravilni_ugibi(self):
+        return " ".join(self.napacne_crke())
 
+    def ugibaj(self, crka):
+        crka = crka.upper()
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+        if crka in self.geslo:
+            self.crke.append(crka)
+            if self.zmaga():
+                return ZMAGA
+            return PRAVILNA_CRKA
+        #potem je napacna neponovljena
+        self.crke.append(crka)
+        if self.poraz():
+            return PORAZ
 
+with open('vaja9/vislice/besede.txt')as f :
+    bazen_besed = [beseda.strip() for beseda in f.readlines()]
 
-# igra = Igra("nekaj")
-# igra.crke = ['a', 'l', 'v','n']
-# print(igra.napacne_crke())
-# print(igra.pravilne_crke())
-# print(igra.stevilo_napak())
-# print(igra.zmaga())
-#
+print(bazen_besed[0])
+print(bazen_besed[-1])
+
+igra = Igra("nekaj")
+igra.crke = ['A', 'L', 'V','N']
+print(igra.napacne_crke())
+print(igra.pravilne_crke())
+print(igra.stevilo_napak())
+print(igra.zmaga())
+print(igra.poraz())
+print(igra.pravilni_del_gesla())
+print(igra.nepravilni_ugibi())
+print(igra.ugibaj('k'))
+#tukej nejkej manjka 
