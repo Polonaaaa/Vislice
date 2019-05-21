55d10169d9e342945f5856acc19138d4d76e1330
Polonaaaa <polona.polonaca@gmail.com>
2019-05-21 15:43:31 +0200

    dodali smo bottle

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
 
+ZACETEK = 'S'
 ZMAGA = 'W'
 PORAZ = 'X'
 
@@ -13,20 +14,20 @@ with open('vaja9/Vislice/besede.txt') as f:
 
 class Igra:
     def __init__(self, geslo):
-        self.geslo = geslo
+        self.geslo = geslo.upper()
         self.crke = []
     
     def napacne_crke(self):
-        return [c for c in self.crke if c not in self.geslo.upper()]
+        return [c for c in self.crke if c not in self.geslo()]
 
     def pravilne_crke(self):
-        return [c for c in self.crke if c in self.geslo.upper()]
+        return [c for c in self.crke if c in self.geslo()]
 
     def stevilo_napak(self):
         return len(self.napacne_crke())
 
     def zmaga(self):
-        return all(c in self.crke for c in self.geslo.upper())
+        return all(c in self.crke for c in self.geslo())
 
     def poraz(self):
         return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
@@ -57,10 +58,40 @@ class Igra:
         self.crke.append(crka)
         if self.poraz():
             return PORAZ
+        else:
+            return NAPACNA_CRKA
 
 def nova_igra():
     geslo = random.choice(bazen_besed)
     return Igra(geslo)
+
+
+class Vislice:
+    def __init__(self):
+        self.igre = {}
+
+    def prost_id_igre(self):
+        return len(self.igre)
+
+    def nova_igra(self):
+        id = self.prost_id_igre()
+        self.igre[id] = (nova_igra(), ZACETEK)
+        return id
+
+    def ugibaj(self, id_igre,crka):
+        igra, _= self.igre[id_igre]
+        poskus = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra, poskus)
+
+        
+
+#########################################################
+v =Vislice()
+v.nova_igra()
+v.nova_igra()
+print(v.igre)
+
+
 
 # print(bazen_besed[0])
 # print(bazen_besed[-1])
